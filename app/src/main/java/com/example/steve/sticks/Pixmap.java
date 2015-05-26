fef5cd43d300b8a298aa69e89d211a8c74b110cb
endreszsteve <endreszsteve@gmail.com>
2015-05-26 17:21:33 -0400

    framework included impl included gl included math included

---
+++ app/src/main/java/com/example/steve/sticks/Pixmap.java
@@ -0,0 +1,13 @@
+package com.example.steve.sticks;
+
+import com.badlogic.androidgames.framework.Graphics.PixmapFormat;
+
+public interface Pixmap {
+    public int getWidth();
+
+    public int getHeight();
+
+    public PixmapFormat getFormat();
+
+    public void dispose();
+}
