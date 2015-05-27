51335f010cebc85c77bea712a690ee713b6e9358
endreszsteve <endreszsteve@gmail.com>
2015-05-27 19:56:45 -0400

    working on renderer

--- app/src/main/java/com/example/steve/sticks/game/WorldRenderer.java
+++ app/src/main/java/com/example/steve/sticks/game/WorldRenderer.java
@@ -25,5 +25,38 @@ public class WorldRenderer
         this.batcher = batcher;
     }
 
+    public void render()
+    {
+        if(world.player.position.y > cam.position.y)
+            cam.position.y = world.player.position.y;
+        cam.setViewportAndMatrices();
+        renderBackground();
+        renderObjects();
+    }
 
+    public void renderBackground()
+    {
+        batcher.beginBatch(Assets.background);
+        batcher.drawSprite(cam.position.x, cam.position.y, FRUSTRUM_WIDTH, FRUSTRUM_HEIGHT, Assets.backgroundRegion);
+        batcher.endBatch();
+    }
+
+    public void renderObjects()
+    {
+        GL10 gl = glGraphics.getGL();
+        gl.glEnable(GL10.GL_BLEND);
+        gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+
+        batcher.beginBatch(Assets.items);
+        renderPlayer();
+        renderOpponent();
+        renderRing();
+        batcher.endBatch();
+        gl.glDisable(GL10.GL_BLEND);
+    }
+
+    private void renderPlayer()
+    {
+
+    }
 }
